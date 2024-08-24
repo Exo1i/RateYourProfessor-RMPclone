b8925a909ddaa63d23d47009a294aac9e0eadd6f
Exo <121557650+Exo1i@users.noreply.github.com>
2024-08-24 20:48:28 +0300

    Search Bar Implemented + UI Changes

--- src/app/Components/TopProfessors.js
+++ src/app/Components/TopProfessors.js
@@ -38,29 +38,35 @@ export default function TopProfessors({onProfessorSelect}) {
         return colors[index % colors.length];
     };
 
-    return (<div className="w-full px-8 mb-20">
-        <h2 className="text-3xl font-bold mb-4 text-center">Top Searched</h2>
-        <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
-            {loading ? (Array(3).fill().map((_, index) => (
-                <div key={index} className="bg-white rounded-lg shadow-lg p-4">
-                    <Skeleton height={150} />
-                    <Skeleton height={30} width="80%" className="mt-4" />
-                    <Skeleton height={20} width="60%" className="mt-2" />
-                    <Skeleton height={20} width="40%" className="mt-1" />
-                </div>))) : (topSearched.map((professor, index) => (<div
-                key={professor.id}
-                className={`rounded-lg shadow-lg p-4 transform transition-transform duration-300 hover:scale-105 hover:shadow-xl cursor-pointer ${getColorClass(index)}`}
-                onClick={() => {
-                    onProfessorSelect(professor.id)
-                    fetchTopSearched(false);
+    return (
+        <div className={'mt-3'}>
+            <h1 className="text-3xl font-bold mb-4 text-center">Top Searched</h1>
+            <div className="overflow-y-auto w-[80vw] mb-5 max-h-[600px]">
+                <div className="grid grid-cols-1 gap-6 md:grid-cols-3 w-full px-8 mb-20">
+                    {loading ? (Array(3).fill().map((_, index) => (
+                        <div key={index} className="bg-white rounded-lg shadow-lg p-4">
+                            <Skeleton height={150} />
+                            <Skeleton height={30} width="80%" className="mt-4" />
+                            <Skeleton height={20} width="60%" className="mt-2" />
+                            <Skeleton height={20} width="40%" className="mt-1" />
+                        </div>))) : (topSearched.map((professor, index) => (<div
+                        key={professor.id}
+                        className={`rounded-lg shadow-lg p-4 transform transition-transform duration-300 hover:scale-105 hover:shadow-xl cursor-pointer ${getColorClass(index)}`}
+                        onClick={() => {
+                            onProfessorSelect(professor.id)
+                            fetchTopSearched(false);
 
-                }}
-            >
-                <h2 className="text-2xl font-semibold mt-4">{professor.firstName} {professor.lastName}</h2>
-                <p className="text-gray-500 text-sm">{professor.department} Department</p>
-                <p className="text-gray-600 text-sm mt-2">Avg. Rating: {professor.avgRating}</p>
-                <p className="text-gray-600 text-sm mt-1">Avg. Difficulty: {professor.avgDifficulty}</p>
-            </div>)))}
+                        }}
+                    >
+                        <h2 className="text-2xl font-semibold mt-4">{professor.firstName} {professor.lastName}</h2>
+                        <p className="text-gray-500 text-sm">{professor.department} Department</p>
+                        <p className="text-gray-600 text-sm mt-2">Avg. Rating: {professor.avgRating}</p>
+                        <p className="text-gray-600 text-sm mt-1">Avg. Difficulty: {professor.avgDifficulty}</p>
+                    </div>)))}
+
+                </div>
+            </div>
         </div>
-    </div>);
+    )
+        ;
 }
